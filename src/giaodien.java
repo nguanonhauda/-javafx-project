7caf5fd1f83615d84fa22e256ebd4919d485a2ef
nguanonhauda <23010359@st.phenikaa-uni.edu.vn>
2024-04-05 14:41:08 +0700

    KK

--- src/giaodien.java
+++ src/giaodien.java
@@ -3,9 +3,5 @@ import javafx.scene.control.Alert;
 
 
 public class giaodien{
-    public void Summit (ActionEvent event){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION) ; 
-        alert.setContentText("Xem");
-        alert.show();
-    }
+    
 }
